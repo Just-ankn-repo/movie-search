15e82800084df67d5e8f593711e16d62601315f7
Andrey Knyshenko <andrei.knyshenko@gmail.com>
2020-05-07 11:37:18 +0400

    feat: add slide template with waiting image preloading

---
+++ movie-search/src/js/templates/slideTpl.js
@@ -0,0 +1,15 @@
+/* global document */
+
+export default (_element) => new Promise((resolve) => {
+  const slide = document.createElement('div');
+  slide.classList.add('swiper-slide');
+  slide.innerHTML = `<div class="slide__container">
+                        <img class="slide__img" src="${_element.Poster}">
+                        <div class="slide__bottom">
+                          <a class="slide__title" href="https://www.imdb.com/title/${_element.imdbID}/videogallery/" target="_blank">${_element.Title}</a>
+                          <p class="slide__year">Year: ${_element.Year}</p>
+                          <p class="slide__rating">ImDb: ${_element.imdbRating}</p>
+                        </div>
+                    </div>`;
+  slide.querySelector('.slide__img').onload = () => resolve(slide);
+});
