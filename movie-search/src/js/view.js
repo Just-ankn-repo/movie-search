ee48b3b8f8a647a9cb21a8015129344e71a61863
Andrey Knyshenko <andrei.knyshenko@gmail.com>
2020-05-07 13:12:39 +0400

    fix: fix first circle loader on initializing app

--- movie-search/src/js/view.js
+++ movie-search/src/js/view.js
@@ -15,21 +15,17 @@ export default class View {
     this.on = $on;
     this.slideTpl = $slideTpl;
     this.swiper = $swiper;
-    this.loader = document.getElementById('page-loader');
     this.data = {};
-    this.swipperWrapper = document.getElementById('swiper-wrapper');
+    this.initial = true;
     this.nextPage = () => this.controller.nextPage();
     this.init();
   }
 
   init() {
-    this.loader.style.display = '';
     this.on.$onSearch(this.controller);
-    this.on.$onImgLoad(this.swiper);
   }
 
   async render(data, update) {
-    this.loader.style.display = '';
     this.swiper.off('reachEnd', this.nextPage);
 
     try {
@@ -39,11 +35,14 @@ export default class View {
       if (update === true && await newSlides) this.swiper.removeAllSlides();
 
       this.swiper.appendSlide(await newSlides);
-
-      this.loader.style.display = 'none';
       this.swiper.on('reachEnd', this.nextPage);
     } catch (error) {
       throw new Error(error);
+    }
+
+    if (this.initial === true) {
+      document.getElementById('page-loader').style.display = 'none';
+      this.initial = false;
     }
   }
 }
