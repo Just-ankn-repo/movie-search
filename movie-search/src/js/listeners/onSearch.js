03a8cdd04317bd7381f1f0330e699ea74fabf750
Andrey Knyshenko <andrei.knyshenko@gmail.com>
2020-05-09 18:19:49 +0400

    feat: add search by Enter key

--- movie-search/src/js/listeners/onSearch.js
+++ movie-search/src/js/listeners/onSearch.js
@@ -6,13 +6,20 @@ export default (controller) => {
   const searchButton = document.getElementById('search-button');
   const searchInput = document.getElementById('search-input');
 
-  const search = () => {
+  const bySearchButton = () => {
     if (searchInput.value.length !== 0) {
       controller.setPage(searchInput.value);
     }
   };
 
+  const byEnterKey = (event) => {
+    if (event.key === 'Enter') {
+      searchButton.click();
+    }
+  };
+
   (function onSearch() {
-    $on(searchButton, 'click', search);
+    $on(searchButton, 'click', bySearchButton);
+    $on(searchInput, 'keydown', byEnterKey);
   }());
-}
+};
