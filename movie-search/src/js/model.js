b94ec8546c9984ea48ec2de8e458e6e9034bce70
Andrey Knyshenko <andrei.knyshenko@gmail.com>
2020-05-12 20:50:21 +0400

    refactor: refactor model class for more readability

--- movie-search/src/js/model.js
+++ movie-search/src/js/model.js
@@ -3,39 +3,34 @@ import $errors from './utils/errors';
 
 export default class Model {
   constructor() {
-    this.data = {};
-    this.rating = '';
     this.apiKey = 'c246be43';
     // this.apiKey = 'fc604359';
     this.apiUrl = `https://www.omdbapi.com/?apikey=${this.apiKey}`;
   }
 
-  async searchMovies(search, page) {
+  async getMovies(search, page) {
     let response;
+
     try {
       response = await fetch(`${this.apiUrl}&s=${search}&page=${page}`);
     } catch (error) {
       throw new Error($errors.connectionError());
     }
-    this.data = await response.json();
-    if (!response.ok || this.data.Error) {
-      throw new Error($errors.apiErrors(this.data.Error, search));
+
+    const moviesList = await response.json();
+
+    if (!response.ok || moviesList.Error) {
+      throw new Error($errors.apiErrors(moviesList.Error, search));
     } else {
-      const newData = this.data.Search.map(async (item) => {
-        const newItem = item;
-        newItem.imdbRating = await this.searchRating(item.imdbID);
-        return newItem;
-      });
-      return Promise.all(newData);
+      return await Promise.all(moviesList.Search.map((movie) => this.addImdbRating(movie)))
     }
   }
 
-  async searchRating(imdbID) {
-    const response = await fetch(`${this.apiUrl}&i=${imdbID}&plot=short`);
-    this.rating = await response.json();
-    if (this.rating.Response === 'False') {
-      return this.rating.Error;
-    }
-    return this.rating.imdbRating;
+  async addImdbRating(movie) {
+    const movieInfo = movie;
+    const response = await fetch(`${this.apiUrl}&i=${movieInfo.imdbID}&plot=short`);
+    const movieDetails = await response.json();
+    movieInfo.imdbRating = movieDetails.imdbRating || movieDetails.Error;
+    return movieInfo;
   }
 }
